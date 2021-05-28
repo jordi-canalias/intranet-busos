c3d23a8d4e2b5bda6d6956f7aa4265afaa7bd849
alina <alina.rebguna@gmail.com>
2021-05-28 15:46:25 +0200

    parada updated

--- frontend_intranet/src/app/landing/landing.component.ts
+++ frontend_intranet/src/app/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../_services/user';
+import { UserService } from '../_services/user.service';
 
 
 @Component({
@@ -11,7 +12,7 @@ export class LandingComponent implements OnInit {
  
   user: User;
   logged: boolean;
-  constructor() { }
+  constructor(public userService: UserService) { }
 
   ngOnInit(): void {
     if (localStorage.getItem("token") == null) {
@@ -19,6 +20,16 @@ export class LandingComponent implements OnInit {
     } else {
       this.logged = true;
     }
+
+    this.userService.getUserByName( localStorage.getItem("user")).subscribe(
+      (result) => {
+        console.log(this.user=result);
+        this.user=result;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 
   }
   logout(): void {
